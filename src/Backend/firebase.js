1005145ba16481f15f8f725e5d85696dc446dbd3
Coby232 <selormkn2003@gmail.com>
2023-10-23 17:48:23 +0000

    signIn/Up

--- src/Backend/firebase.js
+++ src/Backend/firebase.js
@@ -1,21 +1,15 @@
-import { initializeApp } from "firebase/app";
 import {
-  getFirestore,
-  collection,
-  getDocs,
-} from "firebase/firestore/lite";
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
-// TODO: Replace the following with your app's Firebase project configuration
-// Import the functions you need from the SDKs you need
+  getAuth,
+  RecaptchaVerifier,
+  getReactNativePersistence,
+} from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Get the Firebase auth instance
+
+// Export auth
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyDe7VB0-WkFEFMgx-_H0Mw195aE2rNI4mE",
   authDomain: "e-commerce-app-75748.firebaseapp.com",
@@ -23,18 +17,14 @@ const firebaseConfig = {
   storageBucket: "e-commerce-app-75748.appspot.com",
   messagingSenderId: "571476852217",
   appId: "1:571476852217:web:a3e90b79047d2053c4d128",
-  measurementId: "G-Q8S0MZW67C"
+  measurementId: "G-Q8S0MZW67C",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
+// const analytics = getAnalytics(app);
 
-// Get a list of cities from your database
-// async function getCities(db) {
-//   const citiesCol = collection(db, "cities");
-//   const citySnapshot = await getDocs(citiesCol);
-//   const cityList = citySnapshot.docs.map((doc) => doc.data());
-//   return cityList;
-// }
+// Get the Firebase auth instance
+const auth = getAuth(app);
+// Export auth
+export { auth };
--- src/screens/SignIn.jsx
+++ src/screens/SignIn.jsx
@@ -1,11 +1,106 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../Backend/firebase";
+import { useState, useEffect } from "react";
+import HomePage from "./HomePage";
+import { useNavigation } from "@react-navigation/native";
+// import { TextInput } from "react-native-gesture-handler";
 
-const SignIn = () => {
+const SignIn = ({ navigate }) => {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleAuthentication = async () => {
+    if (email.trim() === "" || password.trim() === "") {
+      Alert.alert("Please enter email and password");
+      return;
+    }
+    if (password.length < 6) {
+      Alert.alert("Password should be at least 6 characters");
+      return;
+    }
+
+    try {
+        // Try to sign in the user with email and password
+        await signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log("Signed in with:", user.email);
+            // Navigate to the HomePage
+            navigation.navigate("HomePage");
+          })
+          .catch(async (error) => {
+            // If sign in fails, try to create a new user with email and password
+            if ((error.code === "auth/wrong-password") ) {
+              Alert.alert("Error", "Wrong password. Please try again.");
+            } else if (
+              (error.code === "auth/user-not-found") ||
+              (error.code === "auth/invalid-login-credentials")
+            ) {
+              try {
+                const userCredentials =
+                  await createUserWithEmailAndPassword(
+                    auth,
+                    email,
+                    password
+                  );
+                const user = userCredentials.user;
+                console.log("Registered with:", user.email);
+
+                // Navigate to the HomePage
+                navigation.navigate("HomePage");
+              } catch (error) {
+                console.error(error);
+              }
+            } else {
+              console.error(error);
+            }
+          });
+      } catch (error) {
+        console.error(error);
+      }
+  };
+
   return (
-    <View className='bg-white h-screen'>
-      <View className='flex flex-row justify-center items-center'>
-        <Text>Email</Text>
+    <View className='bg-white h-screen flex flex-row justify-center items-center'>
+      <View className='flex flex-col gap-2'>
+        <TextInput
+          placeholder='Enter your email'
+          className='h-10 min-w-max border-2 p-2'
+          onChangeText={(value) => setEmail(value)}
+          autoCorrect={false}
+          autoCapitalize='none'
+          autoComplete='email'
+        />
+        <TextInput
+          placeholder='Enter your password'
+          className='h-10 min-w-max border-2 p-2'
+          onChangeText={(value) => setPassword(value)}
+          autoCorrect={false}
+          autoCapitalize='none'
+          autoComplete='current-password'
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          className='bg-black flex flex-row justify-center items-center p-2'
+          onPress={() => {
+            handleAuthentication();
+          }}>
+          <Text className='text-white'>Sign In</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
